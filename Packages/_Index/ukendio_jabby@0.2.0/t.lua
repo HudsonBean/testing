ecf2490667e49e569100dce1f382c9b8b6b50fae
HudsonBean <hudsonbeandev@gmail.com>
2026-06-18 12:41:46 -0500

    Downgraded to an older version of jecs to match the internal jecs version in replecs. Fixed a bug where I didn't preregister replecs components before world creation causing some weird errors with timers.

--- Packages/_Index/ukendio_jabby@0.2.0/t.lua
+++ Packages/_Index/ukendio_jabby@0.2.0/t.lua
@@ -1,1 +1,5 @@
-return require(script.Parent.Parent["corecii_greentea@0.4.11"]["greentea"])
+local REQUIRED_MODULE = require(script.Parent.Parent["corecii_greentea@0.4.11"]["greentea"])
+export type Cause = REQUIRED_MODULE.Cause 
+export type Type = REQUIRED_MODULE.Type 
+export type TuplePacked<T...> = REQUIRED_MODULE.TuplePacked<T...>
+return REQUIRED_MODULE
--- Packages/planck-runservice.lua
+++ Packages/planck-runservice.lua
@@ -1,1 +1,4 @@
-return require(script.Parent._Index["yetanotherclown_planck-runservice@0.2.0"]["planck-runservice"])
+local REQUIRED_MODULE = require(script.Parent._Index["yetanotherclown_planck-runservice@0.2.0"]["planck-runservice"])
+export type Phases = REQUIRED_MODULE.Phases 
+export type Pipelines = REQUIRED_MODULE.Pipelines 
+return REQUIRED_MODULE
