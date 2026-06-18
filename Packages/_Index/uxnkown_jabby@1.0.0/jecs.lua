f60ac63e5ee623f87d1908f08610e864500aa2c5
HudsonBean <hudsonbeandev@gmail.com>
2026-06-17 19:45:07 -0500

    Refactoring

--- Packages/_Index/uxnkown_jabby@1.0.0/jecs.lua
+++ Packages/_Index/uxnkown_jabby@1.0.0/jecs.lua
@@ -1,7 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent.Parent["ukendio_jecs@0.3.2"]["jecs"])
-export type Archetype = REQUIRED_MODULE.Archetype 
-export type Id<T > = REQUIRED_MODULE.Id<T >
-export type Pair = REQUIRED_MODULE.Pair 
-export type Entity<T > = REQUIRED_MODULE.Entity<T >
-export type World = REQUIRED_MODULE.World 
-return REQUIRED_MODULE
+return require(script.Parent.Parent["ukendio_jecs@0.3.2"]["jecs"])
