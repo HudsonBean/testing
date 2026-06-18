02dbe9b558cb262f4b6ac7ff1874e09672411ba7
Hudson Bean <hudsonbeandev@gmail.com>
2026-06-18 02:26:05 -0500

    added world and scheduler

--- Packages/_Index/ukendio_jabby@0.2.0/jecs.lua
+++ Packages/_Index/ukendio_jabby@0.2.0/jecs.lua
@@ -1,1 +1,7 @@
-return require(script.Parent.Parent["ukendio_jecs@0.4.0"]["jecs"])
+local REQUIRED_MODULE = require(script.Parent.Parent["ukendio_jecs@0.4.0"]["jecs"])
+export type Archetype = REQUIRED_MODULE.Archetype 
+export type Id<T > = REQUIRED_MODULE.Id<T >
+export type Pair<First, Second> = REQUIRED_MODULE.Pair<First, Second>
+export type Entity<T > = REQUIRED_MODULE.Entity<T >
+export type World = REQUIRED_MODULE.World 
+return REQUIRED_MODULE
