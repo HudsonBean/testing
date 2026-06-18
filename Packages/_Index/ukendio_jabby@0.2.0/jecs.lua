a3fbe3cb9db4616800f00f0f4e495d48e53fc758
HudsonBean <hudsonbeandev@gmail.com>
2026-06-18 11:03:29 -0500

    Finished both the replecsClient and replecsServer files I believe the replication system is finished.

--- Packages/_Index/ukendio_jabby@0.2.0/jecs.lua
+++ Packages/_Index/ukendio_jabby@0.2.0/jecs.lua
@@ -1,7 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent.Parent["ukendio_jecs@0.4.0"]["jecs"])
-export type Archetype = REQUIRED_MODULE.Archetype 
-export type Id<T > = REQUIRED_MODULE.Id<T >
-export type Pair<First, Second> = REQUIRED_MODULE.Pair<First, Second>
-export type Entity<T > = REQUIRED_MODULE.Entity<T >
-export type World = REQUIRED_MODULE.World 
-return REQUIRED_MODULE
+return require(script.Parent.Parent["ukendio_jecs@0.4.0"]["jecs"])
--- Packages/_Index/ukendio_jabby@0.2.0/t.lua
+++ Packages/_Index/ukendio_jabby@0.2.0/t.lua
@@ -1,5 +1,1 @@
-local REQUIRED_MODULE = require(script.Parent.Parent["corecii_greentea@0.4.11"]["greentea"])
-export type Cause = REQUIRED_MODULE.Cause 
-export type Type = REQUIRED_MODULE.Type 
-export type TuplePacked<T...> = REQUIRED_MODULE.TuplePacked<T...>
-return REQUIRED_MODULE
+return require(script.Parent.Parent["corecii_greentea@0.4.11"]["greentea"])
